31185ccdd73bb0258a0b187fde1f996626d7d600
mercuryeater <federicoc03@gmail.com>
2024-12-01 12:31:06 -0500

    Enhance ChatWindow component with auto-scrolling feature; update styles for improved aesthetics and user experience

--- src/components/molecules/ChatWindow/ChatWindow.tsx
+++ src/components/molecules/ChatWindow/ChatWindow.tsx
@@ -1,12 +1,15 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useChat } from "ai/react";
 
 import MessageBubble from "@/components/atoms/MessageBubble/MessageBubble";
 import LoadingBubble from "@/components/atoms/LoadingBubble/LoadingBubble";
 
 import "./chatWindow.scss";
+
 const ChatWindow: React.FC = () => {
+  const messagesContainerRef = useRef(null);
+
   const { isLoading, messages, input, handleInputChange, handleSubmit } =
     useChat({
       onResponse: (response) => {
@@ -14,11 +17,19 @@ const ChatWindow: React.FC = () => {
       },
     });
 
+  useEffect(() => {
+    // Scroll to the bottom whenever messages change
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTop =
+        messagesContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
     <div className="chatWindow">
       <>
-        <div className="messagesContainer">
-          {mockMessages.map((message, index) => (
+        <div ref={messagesContainerRef} className="messagesContainer">
+          {messages.map((message, index) => (
             <MessageBubble key={`message-${index}`} message={message} />
           ))}
         </div>
@@ -41,8 +52,9 @@ export default ChatWindow;
 
 const mockMessages = [
   {
-    content: "Hello! How can I help you today?",
-    role: "bot",
+    content:
+      '¡Por supuesto! Aquí tienes un ejemplo que mezcla texto normal, markdown y algo de código:\n\n---\n\n**Un Día en Usaquén**:\n\n*Por la mañana*:\n- **7:00 AM**: **Despierta** y disfruta una taza de _café colombiano recién hecho_. ☕\n\n*Por la tarde*:\n- **1:00 PM**: Disfruta un **almuerzo** delicioso en un puesto local de _arepas_. 🌮\n\n*Por la noche*:\n- **6:00 PM**: Da un paseo tranquilo por las pintorescas calles de Usaquén.\n\n```python\n# Ejemplo de código simple en Python\ndef saludos(nombre):\n    return f"Hola, {nombre}! Bienvenido a Usaquén."\n\nnombre_usuario = "Amigo"\nprint(saludos(nombre_usuario))\n```\n\n---\n\nEspero que esto te muestre cómo puedes combinar texto normal, markdown y código para crear contenido atractivo y estructurado. 😊',
+    role: "assistant",
   },
   {
     content: "I need help with my order",
@@ -50,6 +62,8 @@ const mockMessages = [
   },
   {
     content: "sss",
-    role: "bot",
+    role: "assistant",
   },
 ];
+
+const mockMarkdown = `# ¡Hola! 👋 `;
